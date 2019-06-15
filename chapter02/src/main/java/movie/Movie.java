5f94b048ac3bd2984c139a3488652f855fc0d04b
hyecheon <rainbow880616@gmail.com>
2019-06-15 20:38:50 +0900

    object 2장

---
+++ chapter02/src/main/java/movie/Movie.java
@@ -0,0 +1,13 @@
+package movie;
+
+import money.Money;
+
+public class Movie {
+    public Money getFee() {
+        return null;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return null;
+    }
+}
